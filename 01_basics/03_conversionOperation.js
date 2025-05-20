f46e4f713180359b2a4d47d05200ec110ea1fae7
Vedant103045 <vedantsshinde1045@gmail.com>
2025-05-20 06:10:20 +0000

    Conversion of Datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = false
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber);
+
+// "30" => 33
+// "33vedant"=> NaN
+// true => 1
+// false => 0
+
+let isLoggedIn = "Vedant"
+
+let booleanIsLoggedin = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedin);
+
+// 1 ko boolean mein convert karenge to true aayega
+// 0 => false
+// "" => false
+// "vedant" => true
+
+let number = 103045
+
+let stringNumber = String(number)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
